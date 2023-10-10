981e0664d74708656cd5aad18c7f1712778cf139
Макс Конончук <eexet17@gmail.com>
2023-10-10 22:43:19 +0300

    updated

--- IMapClient/MainWindow.xaml.cs
+++ IMapClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MailKit.Net.Imap;
+﻿using MailKit;
+using MailKit.Net.Imap;
 using MailKit.Security;
 using System;
 using System.Windows;
@@ -9,26 +10,34 @@ namespace IMapClient
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {   
+    {
         string myEmailAddress;
         string accountPassword;
-        private ImapClient client = new();
-        
+        private ImapClient client;
+
         public MainWindow(string login, string pass)
         {
             InitializeComponent();
+
             myEmailAddress = login;
             accountPassword = pass;
+            client = new ImapClient();
         }
-         
-        private async void Button_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListFolders();
+        }
+        private async void ListFolders()
         {
             try
             {
                 await client.ConnectAsync("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
                 await client.AuthenticateAsync(myEmailAddress, accountPassword);
 
-                foreach (var fl in client.GetFolders(client.PersonalNamespaces[0]))
+                //listBox.Items.Clear();
+
+                foreach (var fl in await client.GetFoldersAsync(client.PersonalNamespaces[0]))
                 {
                     listBox.Items.Add(fl.Name);
                 }
@@ -54,5 +63,5 @@ namespace IMapClient
             }
             Application.Current.Shutdown();
         }
-    }                                           
+    }
 }
